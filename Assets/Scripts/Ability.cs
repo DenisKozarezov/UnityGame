9564c16280d9622418f8deacc04730248df871e3
DenisKozarezov <kozarezov.d.v@mail.ru>
2020-02-28 00:00:47 +0300

    Обновление
    На всякий случай написал прототип для предметов и способностей.

---
+++ Assets/Scripts/Ability.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbilitiesBank
+{
+    public static Ability [] Abilities = new Ability[10];
+
+    public static Ability GetAbilityByName(string _name)
+    {
+        foreach (Ability ability in Abilities)
+        {
+            if (ability != null && ability.Name == _name) return ability;
+        }
+        return null;
+    }
+}
+public class Ability
+{    
+    public enum AbilityType { TARGET, NONTARGET, POINT }
+    public string Name { set; get; }
+    public string Description { set; get; }
+    public float Cooldown { set; get; }
+    public Unit Target { set; get; }
+    public Vector2 Point { set; get; }
+    public AbilityType Type { set; get; } = AbilityType.NONTARGET;
+
+    private delegate void TargetAction(Unit _target);
+    private delegate void NonTargetAction();
+    private delegate void PointAction(Vector2 _point);
+
+    TargetAction TDelegate;
+    NonTargetAction NTDelegate;
+    PointAction PDelegate;
+
+    private void AddAction(TargetAction _action)
+    {
+        TDelegate = _action;
+    }
+    private void AddAction(NonTargetAction _action)
+    {
+        NTDelegate = _action;
+    }
+    private void AddAction(PointAction _action)
+    {
+        PDelegate = _action;
+    }
+    public void Cast()
+    {
+        switch (Type)
+        {
+            case AbilityType.TARGET:
+                TDelegate.Invoke(Target);
+                break;
+            case AbilityType.POINT:
+                PDelegate.Invoke(Point);
+                break;
+            case AbilityType.NONTARGET:
+                NTDelegate.Invoke();
+                break;
+        }
+    }
+}
+
+
+/* Действия способностей */
+static class Methods
+{
+    
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ItemsBank
+{
+    public static Item[] Items = new Item[10];
+
+    public static Item GetItemByName(string _name)
+    {
+        foreach (Item item in Items)
+        {
+            if (item != null & item.Name == _name) return item;
+        }
+        return null;
+    }
+}
+public class Item : MonoBehaviour
+{
+    public enum ItemType { HasCharges, Disposable, Usable }
+    public string Name { set; get; }
+    public string Description { set; get; }
+    public int Charges { set; get; }
+    ItemType Type { set; get; }
+
+    public delegate void ChargesAction();
+    public delegate void UsableAction();
+
+    ChargesAction CAction;
+    UsableAction UAction;
+
+    public void AddAction(ChargesAction _action)
+    {
+        CAction = _action;
+    }
+    public void AddAction(UsableAction _action)
+    {
+        UAction = _action;
+    }
+    public void Use()
+    {
+        switch (Type)
+        {
+            case ItemType.Disposable:
+                break;
+            case ItemType.HasCharges:
+                break;
+            case ItemType.Usable:
+                break;
+        }
+    }
+}
