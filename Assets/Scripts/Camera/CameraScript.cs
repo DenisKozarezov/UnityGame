408b3f152f0a8cec34eec15d9503296c27490f00
DenisKozarezov <kozarezov.d.v@mail.ru>
2020-02-27 08:13:09 +0300

    Обновление

--- Assets/Scripts/Camera/CameraScript.cs
+++ Assets/Scripts/Camera/CameraScript.cs
@@ -5,17 +5,12 @@ using UnityEngine;
 [RequireComponent(typeof(CameraScript))]
 public class CameraScript : MonoBehaviour
 {
-    private static float CameraSpeed { set; get; } = 2f;
-    // Start is called before the first frame update
-    void Start()
+    public static float CameraSpeed { private set; get; } = 2f;
+    public static float CameraNormalZoom { private set; get; }
+
+    private void Awake()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        CameraNormalZoom = Camera.main.orthographicSize;
     }
 
     public static void InstanceMoveTo(Vector2 _direction)
@@ -25,14 +20,13 @@ public class CameraScript : MonoBehaviour
     }
     public static void SmoothMoveTo(Vector2 _direction, float _time)
     {
-        Camera.main.gameObject.GetComponent<MonoBehaviour>().StartCoroutine(SmoothMove(_direction, _time));
+        Camera.main.gameObject.GetComponent<MonoBehaviour>().StartCoroutine(InterpolatedMove(_direction, _time));
     }
     public static void SmoothMoveTo(Vector3 _direction, float _time)
     {
-        Camera.main.gameObject.GetComponent<MonoBehaviour>().StartCoroutine(SmoothMove(_direction, _time));
+        Camera.main.gameObject.GetComponent<MonoBehaviour>().StartCoroutine(InterpolatedMove(_direction, _time));
     }
-
-    private static IEnumerator SmoothMove(Vector3 _direction, float _time)
+    private static IEnumerator InterpolatedMove(Vector3 _direction, float _time)
     {
         float distance = Vector2.Distance(new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y), _direction);
         while (Camera.main.gameObject.transform.position != _direction)
@@ -41,6 +35,22 @@ public class CameraScript : MonoBehaviour
             yield return new WaitForEndOfFrame();
             Camera.main.gameObject.transform.position = Vector3.Lerp(Camera.main.transform.position, _direction, deltaTime / distance);
         }
-        Camera.main.gameObject.GetComponent<MonoBehaviour>().StopCoroutine(SmoothMove(_direction, _time));
+        Camera.main.gameObject.GetComponent<MonoBehaviour>().StopCoroutine(InterpolatedMove(_direction, _time));
+    }
+
+    public static void CameraZoom(float _zoom, float _time)
+    {
+        Camera.main.gameObject.GetComponent<MonoBehaviour>().StartCoroutine(InterpolatedZoom(_zoom, _time));
+    }
+    private static IEnumerator InterpolatedZoom(float _zoom, float _time)
+    {
+        float current_time = 0;
+        while (Camera.main.orthographicSize != _time)
+        {
+            yield return new WaitForEndOfFrame();
+            Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, _zoom, current_time / _time);
+            current_time += Time.deltaTime;
+        }
+        Camera.main.gameObject.GetComponent<MonoBehaviour>().StopCoroutine(InterpolatedZoom(_zoom, _time));
     }
 }
