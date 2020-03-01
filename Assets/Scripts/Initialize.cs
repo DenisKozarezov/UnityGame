6a041d11107a0fa3fb05acf35d748f0c19eabf22
DenisKozarezov <kozarezov.d.v@mail.ru>
2020-03-01 14:56:39 +0300

    Обновление

--- Assets/Scripts/Initialize.cs
+++ Assets/Scripts/Initialize.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class Initialize : MonoBehaviour
 {
     public Sprite Background;
@@ -9,6 +10,10 @@ public class Initialize : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Clear();
+        GameObject wall = new GameObject();
+        wall.transform.parent = GameObject.Find("BACKGROUND LAYER").transform;
+        wall.name = "Wall";
         for (int i = 1; i <= 25; i++)
         {
             for (int j = 1; j <= 5; j++)
@@ -17,10 +22,18 @@ public class Initialize : MonoBehaviour
                 obj.AddComponent<SpriteRenderer>().sprite = Background;
                 obj.GetComponent<SpriteRenderer>().material = LightMaterial;
                 obj.transform.position = new Vector2(i * Background.bounds.size.x, j * Background.bounds.size.y);
-                obj.gameObject.layer = 0;
-                obj.gameObject.transform.parent = GameObject.Find("Wall").gameObject.transform;
+                obj.gameObject.layer = 0;                
+                obj.gameObject.transform.parent = wall.gameObject.transform;
                 obj.name = "Background";
             }
+        }
+    }
+    
+    void Clear()
+    {
+        for (int i = 0; i < GameObject.Find("BACKGROUND LAYER").transform.childCount; i++)
+        {
+            DestroyImmediate(GameObject.Find("BACKGROUND LAYER").transform.GetChild(i).gameObject);
         }
     }
 }
